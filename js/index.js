1d47bfd9d6e8d5ba1954dae91576cea42e91e838
Jungsub Shin <supsup5642@gmail.com>
2019-03-28 00:10:11 +0900

    Magnific Popup

--- js/index.js
+++ js/index.js
@@ -1,14 +1,19 @@
-// Turn img alt into caption
-$('.content > p > img[alt]').replaceWith(function () {
-    return '<figure>'
-        + '<a href="' + $(this).attr('src') + '" class="mg-link">'
-        + '<img src="' + $(this).attr('src') + '"/></a>'
-        + '<figcaption class="caption">' + $(this).attr('alt') + '</figcaption>'
-        + '</figure>';
-});
+/* global window */
+(function (window, document, $) {
+    'use strict';
 
- // Connect magnific popup image viewer
-$('.mg-link').magnificPopup({
-    type: 'image',
-    closeOnContentClick: true
-});
+        // turn img alt into caption
+        $('.content > p > img[alt]').replaceWith(function () {
+            return '<figure>'
+                + '<a href="' + $(this).attr('src') + '" class="mg-link">'
+                + '<img src="' + $(this).attr('src') + '"/></a>'
+                + '<figcaption class="caption">' + $(this).attr('alt') + '</figcaption>'
+                + '</figure>';
+        });
+        // and connect magnific popup image viewer
+        $('#post-content .mg-link').magnificPopup({
+            type: 'image',
+            closeOnContentClick: true
+        });
+    });
+}(window, window.document, window.jQuery));
