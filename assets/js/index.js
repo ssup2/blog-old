f880a003377e730218af278796a40f1e4574e7f9
Jungsub Shin <supsup5642@gmail.com>
2019-03-28 00:27:18 +0900

    Magnific Popup

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,14 @@
-/* global window */
-(function (window, document, $) {
-    'use strict';
+// Turn img alt into caption
+$('.content > p > img[alt]').replaceWith(function () {
+    return '<figure>'
+        + '<a href="' + $(this).attr('src') + '" class="mg-link">'
+        + '<img src="' + $(this).attr('src') + '"/></a>'
+        + '<figcaption class="caption">' + $(this).attr('alt') + '</figcaption>'
+        + '</figure>';
+});
 
-    $(function () {
-        // Turn img alt into caption
-        $('.content > p > img[alt]').replaceWith(function () {
-            return '<figure>'
-                + '<a href="' + $(this).attr('src') + '" class="mg-link">'
-                + '<img src="' + $(this).attr('src') + '"/></a>'
-                + '<figcaption class="caption">' + $(this).attr('alt') + '</figcaption>'
-                + '</figure>';
-        });
-
-        // Connect magnific popup image viewer
-        $('#post-content .mg-link').magnificPopup({
-            type: 'image',
-            closeOnContentClick: true
-        });
-    });
-}(window, window.document, window.jQuery));
+// Connect magnific popup image viewer
+$('#post-content .mg-link').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true
+});
